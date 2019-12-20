fdb6bb37febd51c2287037c0dc61ed655787dd21
Andreas Wittmann <mail@andreaswittmann.com>
2019-12-20 09:26:47 +0100

    improve content

--- webapp/src/pages/WrongNetwork.tsx
+++ webapp/src/pages/WrongNetwork.tsx
@@ -6,9 +6,11 @@ export default function WrongNetwork() {
     <Grid container justify="center" spacing={2}>
       <Grid item xs={12} md={8}>
         <Typography variant="h1" paragraph={true}>
-          How to use dwitter
+          Wrong ethereum network
         </Typography>
-        <Typography variant="h2">Ethereum Network: Ropsten</Typography>
+        <Typography variant="h2">
+          Please switch to Ethereum Network: Ropsten
+        </Typography>
         <Typography variant="body1" paragraph={true}>
           Dwitter is currently only available on ropsten testnet. Please make
           sure that your wallet is connected to the ropsten testnet and not to
