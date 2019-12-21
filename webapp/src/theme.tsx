f46ceb578f83ad624327b0aeb5787cdc3229bc45
Andreas Wittmann <mail@andreaswittmann.com>
2019-12-21 18:55:18 +0100

    override card font sizes

--- webapp/src/theme.tsx
+++ webapp/src/theme.tsx
@@ -8,6 +8,18 @@ const theme = createMuiTheme({
     h2: {
       fontSize: "2rem"
     }
+  },
+  overrides: {
+    MuiCardHeader: {
+      title: {
+        fontSize: "1rem"
+      }
+    },
+    MuiCardContent: {
+      root: {
+        fontSize: "1.2rem"
+      }
+    }
   }
 });
 
