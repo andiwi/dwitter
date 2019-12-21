d5ca437d5c1d8ea01ea5e42e052145dab37a50fa
Andreas Wittmann <mail@andreaswittmann.com>
2019-12-21 18:20:52 +0100

    Updates

---
+++ precache-manifest.b6edc11c62778c16d9c986abdbf3f6f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cac6bbc673eab16631855b5b18e3f5",
+    "url": "/dwitter/index.html"
+  },
+  {
+    "revision": "7ced16144a32cf9ac21b",
+    "url": "/dwitter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f3a3a7e2237768cffab5",
+    "url": "/dwitter/static/js/2.3225e354.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/dwitter/static/js/2.3225e354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ced16144a32cf9ac21b",
+    "url": "/dwitter/static/js/main.3b49a746.chunk.js"
+  },
+  {
+    "revision": "98f22c669afdd83e654c",
+    "url": "/dwitter/static/js/runtime-main.08e05f99.js"
+  }
+]);
